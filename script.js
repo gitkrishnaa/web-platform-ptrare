c4dd314d9a6efb7359e0ba3c47f3b6b44e6b0cf9
gitkrishnaa <84628880+gitkrishnaa@users.noreply.github.com>
2022-12-09 12:37:07 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
-let a1=document.getElementById("mydiv")
-let a2=document.getElementById("mydiv1")
-console.log(a1)
-a1.addEventListener("click",()=>{
-console.log(a1)
+// let a1=document.getElementById("mydiv")
+// let a2=document.getElementById("mydiv1")
+// console.log(a1)
+// a1.addEventListener("click",()=>{
+// console.log(a1)
 
 
-})
-a1.style.top="370px";
-a1.style.left="80px";
+// })
+// a1.style.top="370px";
+// a1.style.left="80px";
 
-a2.style.top="350px";
-a2.style.left="80px";
+// a2.style.top="350px";
+// a2.style.left="80px";
