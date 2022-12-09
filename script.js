fb0666213536d7b65830083937c48dae5c7937aa
gitkrishnaa <84628880+gitkrishnaa@users.noreply.github.com>
2022-12-09 19:34:43 +0530

    div1 editor in editer menu add

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log('Hello!');
-let arrayOfid=[];
+let arrayOfid = [];
 function head1() {
   let crt1 = document.createElement('div');
   let input1 = document.createElement('input');
@@ -48,26 +48,22 @@ function head1() {
 }
 function subhead1() {
   let crt1 = document.createElement('div');
-  let mainDivid=crt1.id="id"+arrayOfid.length;
-  arrayOfid.push(mainDivid)
+  let mainDivid = (crt1.id = 'id' + arrayOfid.length);
+  arrayOfid.push(mainDivid);
   console.log(mainDivid);
   console.log(arrayOfid);
 
   let input1 = document.createElement('input');
   let head1 = document.createElement('h5');
 
-//delete button 
-let delet1 = document.createElement('button');
-delet1.innerHTML="delete";
-delet1.addEventListener('click', () => {
-  crt1.remove()
-});
+  //delete button
+  let delet1 = document.createElement('button');
+  delet1.innerHTML = 'delete';
+  delet1.addEventListener('click', () => {
+    crt1.remove();
+  });
 
-///////////
-
-
-
-
+  ///////////
 
   //applying main css on created subhead
   // head1.innerText = 'heading1';
@@ -104,50 +100,95 @@ delet1.addEventListener('click', () => {
   crt1.appendChild(btn1);
   crt1.appendChild(delet1);
   crt1.appendChild(head1);
+}
+function box1() {
+  var arrayOfDiv1Id=[]
+  //// creating main editor div - div1\\\\\\\\\\\\\\
+  let div1 = document.createElement('div');
+  div1.style.width="100%"
+div1.style.backgroundColor="red";
+  div1.style.height="100px"
 
+  let preview=document.getElementById("pre1") //div in preview
+  preview.appendChild(div1);
+    //// creating editor for div1  - div1\\\\\\\\\\\\\\
+  let editor1 = document.createElement('div');
+  let editorBox1=document.getElementById("editorBox1")
+  editorBox1.appendChild(editor1)
+  editor1.style.width="100%"
+  editor1.style.backgroundColor="white";
+  editor1.style.height="100px"
+  ///// creating properties of array like height width etc
+
+
+   // div box width\\\\\\
  
+  let div1_Width = document.createElement('input');
+  editor1.appendChild(div1_Width)
+  div1_Width.placeholder="width"
+  div1_Width.addEventListener("input",()=>{
+   div1.style.width=div1_Width.value;
+  })
+  
+  // div box height\\\\\\
+  let div1_height = document.createElement('input');
+  editor1.appendChild(div1_height)
+  div1_height.placeholder="height"
+  div1_height.addEventListener("input",()=>{
+   div1.style.height=div1_height.value;
+  })
+  
+   // div box background color\\\\\\
+   let div1_color_tab = document.createElement('h4');
+   editor1.appendChild(div1_color_tab);
+   div1_color_tab.innerText="pick color"
 
+
+
+  let div1_color = document.createElement('input');
+  editor1.appendChild(div1_color);
+  div1_color.type="color";
+ 
+  // div1_color.placeholder="background color"
+  div1_color.addEventListener("input",()=>{
+   div1.style.backgroundColor=div1_color.value;
+  })
+  
  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // dragElement(document.getElementById("mydiv"));
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
