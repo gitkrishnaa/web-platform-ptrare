5ba96f170838aa19585ba558cc4fb48caece2dcd
gitkrishnaa <84628880+gitkrishnaa@users.noreply.github.com>
2022-12-09 20:51:31 +0530

    now object can move one parent to another parent element

--- script.js
+++ script.js
@@ -101,13 +101,21 @@ function subhead1() {
   crt1.appendChild(delet1);
   crt1.appendChild(head1);
 }
+
+let arrayOfDiv1Id=[];
+let postion_record//this is for record id for div so child element get position at parent id
 function box1() {
-  var arrayOfDiv1Id=[]
+ 
   //// creating main editor div - div1\\\\\\\\\\\\\\
   let div1 = document.createElement('div');
   div1.style.width="100%"
 div1.style.backgroundColor="red";
   div1.style.height="100px"
+// addaing id by array index and store in array
+div1.id="div1_id"+ arrayOfDiv1Id.length
+arrayOfDiv1Id.push(div1.id)
+console.log(arrayOfDiv1Id,"edit at line near 116")
+
 
   let preview=document.getElementById("pre1") //div in preview
   preview.appendChild(div1);
@@ -138,8 +146,8 @@ div1.style.backgroundColor="red";
    div1.style.height=div1_height.value;
   })
   
-   // div box background color\\\\\\
-   let div1_color_tab = document.createElement('h4');
+   /////// div box background color\\\\\\
+   let div1_color_tab = document.createElement('span');
    editor1.appendChild(div1_color_tab);
    div1_color_tab.innerText="pick color"
 
@@ -153,8 +161,56 @@ div1.style.backgroundColor="red";
   div1_color.addEventListener("input",()=>{
    div1.style.backgroundColor=div1_color.value;
   })
+//////////adding delete button\\\\\\\\\\\\\
+  let div1_and_editor_delete = document.createElement('button');
+  editor1.appendChild(div1_and_editor_delete)
+  div1_and_editor_delete.innerText="delete"
+  div1_and_editor_delete.addEventListener("click",()=>{
+   editor1.remove()
+   div1.remove()
+  })
+  ////////// adding hide button for editor1 - it mean this editor not active\\\\\\\\\\\\\
+  let editor1_hide = document.createElement('button');
+  editor1.appendChild(editor1_hide)
+  editor1_hide.innerText="hide me";
+  editor1_hide.addEventListener("click",()=>{
+   editor1.style.display="none";
+    // use 0px border to show unactive or not editor1 and div1
+    div1.style.border="0px"
   
+  })
+  /////// adding function to hide  editor1 on double click on div1 \\\\\\\\\\\\\
+  div1.addEventListener("dblclick",(e)=>{
+    editor1.style.display="none";
+    // use dblclick=double click to show active editor1 and div1
+    div1.style.border="0px"
+
+   
+   })
+
+  /////// adding function to  show the hideend editor1 for div1 \\\\\\\\\\\\\
+  div1.addEventListener("click",(e)=>{
+    editor1.style.display="block";
+    // use border to show active editor1 and div1
+    div1.style.border="2px solid black"
+console.log(e.target)
+console.log(postion_record,"position")
+postion_record=e.target.id;
+console.log(postion_record,"position udated")
+
+   
+   })
+   ////
  
+    /////// adding function to  show the hideend editor1 for div1 \\\\\\\\\\\\\
+   let position1 = document.createElement('button');
+   editor1.appendChild(position1)
+   position1.innerText="click for pos"
+   position1.addEventListener("click",()=>{
+    document.getElementById(postion_record).appendChild(div1);
+   })
+
+
 }
 
 
